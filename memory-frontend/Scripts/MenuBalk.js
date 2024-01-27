8269efb6008ac8b3c8c02dbb5072b26ca6516eb7
Wesley Middel <wesley.middel@fmi-improvia.com>
2024-01-27 14:09:47 +0100

    Page scructure, added highscores

---
+++ memory-frontend/Scripts/MenuBalk.js
@@ -0,0 +1,26 @@
+if (window.self === window.top) {
+  document.write(`
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link btn btn-primary" href="http://localhost:8080/Home/PageHome.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link btn btn-primary" href="http://localhost:8080/Login/PageLogin.html">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link btn btn-primary" href="http://localhost:8080/Game/PageGame.html">Game</a>
+            </li>
+          </ul>
+        </div>
+    </nav>
+  </header>
+`);
+}
+
+
